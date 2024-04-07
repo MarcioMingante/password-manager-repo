136ee67dc6143ef241f1997906a50c4cc1d52738
Márcio Mingante <marciomvp17@gmail.com>
2024-04-07 15:19:38 -0300

    conclui req 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css';
+import Form from './components/Form';
 import Title from './components/Title';
 
 function App() {
   return (
-    <Title />
+    <>
+      <Title />
+      <Form />
+    </>
+
   );
 }
 
